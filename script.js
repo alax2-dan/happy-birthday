3e66b01e2cf596534ae87616f714d6a38846307c
alax2-dan <dchan8294uk@gmail.com>
2025-06-29 22:30:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const buyBtn = document.getElementById("buy-btn");
+const slimBtn = document.getElementById("slim-btn");
+const moneyBtn = document.getElementById("money-btn");
+const supriseBtn = document.getElementById("suprise-btn");
+
+function buyBtnOnClick()
+{
+    window.location.href = "buy.html";
+}
+
+function slimBtnOnClick()
+{
+    window.location.href = "slim.html";
+}
+
+function moneyBtnOnClick()
+{
+    window.location.href = "money.html";
+}
+
+function supriseBtnOnClick()
+{
+    window.location.href = "suprise.html";
+}
